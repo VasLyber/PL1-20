80b3f50575d242fd6f9e4d3e1d959e2969e71ab0
VasLyber <lyberatosbill@hotmail.com>
2020-05-16 20:02:40 +0300

    prologing_v2

--- 2η Άσκηση/PowersOfTwo.pl
+++ 2η Άσκηση/PowersOfTwo.pl
@@ -18,12 +18,38 @@ read_line(Stream, List) :-
       maplist(atom_number, As, List)
     ).
 
+dec2bin(0,[0]).
+dec2bin(1,[1]).
+dec2bin(N,L):-
+    N > 1,
+    X is N mod 2,
+    Y is N // 2,
+    dec2bin(Y,L1),
+    L = [X|L1].
+
+sum_list([], 0).
+sum_list([H|T], Sum) :-
+   sum_list(T, Rest),
+   Sum is H + Rest.
+
+convert(L,M) :-
+  between(1,,B)
 
 
-powers2(File, Answers) :-
-    read_input(File, N, Numbers),
-    stations_from_list(L,N,SL,EL),
-    reverse(EL,REL),
-    merge(SL,REL,Merged),
-    [(K,_)|Tail] = Merged,
-    max_ski(K,K,K,Tail,Result).
+testlist([]).
+testlist([H|T]) :-
+  dec2bin(H,L),
+  sum_list(L,Q),
+  length(L,W),
+  portray_clause(Q),
+  (
+    Q>T -> portray_clause([]),
+    fail
+  ; Q =:= T ->
+    portray_clause(L)
+%  ; convert(L,M), portray_clause(M)
+  ).
+powers2(File) :-
+  read_input(File, Ns, Num),
+  member(X,Num),
+  testlist(X).

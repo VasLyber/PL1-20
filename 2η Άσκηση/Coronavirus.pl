8f390ed30fd0b02a49b9260b62bf6bdc66a52ace
VasLyber <lyberatosbill@hotmail.com>
2020-05-25 20:41:09 +0300

    διαβασμα απο αρχείο για 1 κορωνογραφο.

--- 2η Άσκηση/Coronavirus.pl
+++ 2η Άσκηση/Coronavirus.pl
@@ -1,24 +1,35 @@
-read_input(File, N, Numbers) :-
-  %  open(File, read, Stream),
-  %  read_line(Stream, N),
-    read_lines(Stream, N, Numbers).
-
-read_lines(Stream, N, Numbers) :-
-  ( N == 0 -> Numbers = []
-  ; N > 0  -> read_line(Stream, Number),
-              Nm1 is N-1,
-              read_lines(Stream, Nm1, RestNumbers),
-              Numbers = [Number | RestNumbers]).
-
-read_line(Stream, List) :-
+read_input(File, Term) :-
+    open(File, read, Stream),
     read_line_to_codes(Stream, Line),
-    ( Line = [] -> List = []
-    ; atom_codes(A, Line),
-      atomic_list_concat(As, ' ', A),
-      maplist(atom_number, As, List)
-    ).
+    atom_codes(A, Line),
+    atomic_list_concat(As, ' ', A),
+    maplist(atom_number, As, [KA|LA]),
+    L is LA,
+    K is KA,
+    functor(Term, array, K),
+    read_lines(Stream, L, Term).
 
+read_lines(Stream, N, Term) :-
+  ( N == 0 ->   true
+  ; N > 0  ->   read_line_to_codes(Stream, Line),
+                atom_codes(A, Line),
+                atomic_list_concat(As, ' ', A),
+                maplist(atom_number, As, [KA|LA]),
+                L is LA,
+                K is KA,
+                arg(K,Term,LO),
+                ( not(var(LO)) -> append([L],LO,LI),
+                                  setarg(K, Term, LI)
+                ; var(LO)      -> setarg(K, Term, [L])
+                ),
+                arg(L,Term,KO),
+                ( not(var(KO)) -> append([K],KO,KI),
+                                  setarg(L, Term, KI)
+                ; var(KO)      -> setarg(L, Term, [K])
+                ),
+                Nm1 is N-1,
+                read_lines(Stream, Nm1,Term)
+  ).
 coronograph(File) :-
-	open(File, read, Stream),
-	read_line(Stream, N),
-	read_input(File, Ns, Num),
+  read_input(File,Term),
+  portray_clause(Term) .

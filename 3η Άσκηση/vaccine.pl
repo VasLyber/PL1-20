bf15894aea57ca404abe6d74d6143fdb0e5f1c8c
VasLyber <lyberatosbill@hotmail.com>
2020-07-15 18:45:03 +0300

    Prolog

--- 3η Άσκηση/vaccine.pl
+++ 3η Άσκηση/vaccine.pl
@@ -28,66 +28,50 @@ read_lines(Stream, N, Chars) :-
               Nm1 is N-1,
               read_lines(Stream, Nm1, RestChars),
               Chars = [O | RestChars]).
-complement([],Z,Z).
-complement([H|T],Z,Acc) :-
-  (
-    H == 85 ->  complement(T,Z,[65|Acc])
-  ; H == 65 ->  complement(T,Z,[85|Acc])
-  ; H == 71 ->  complement(T,Z,[67|Acc])
-  ; H == 67 ->  complement(T,Z,[71|Acc])
-  ).
 
-copy(L,R) :- accCp(L,R).
-accCp([],[]).
-accCp([H|T1],[H|T2]) :- accCp(T1,T2).
+replace(_, _, _,_,[], []).
+replace(O, R, W, U, [O|T], [R|T2]) :- replace(O, R, W, U, T, T2).
+replace(O, R, W, U, [R|T], [O|T2]) :- replace(O, R, W, U, T, T2).
+replace(O, R, W, U, [W|T], [U|T2]) :- replace(O, R, W, U, T, T2).
+replace(O, R, W, U, [U|T], [W|T2]) :- replace(O, R, W, U, T, T2).
 
-push([H|T],NewState1,State2,NewState2):-
-  append([H],State2,NewState2),
-  copy(T,NewState1).
 
-action(State1,State2,c,NewState1,NewState2):-
-  copy(State2,NewState2),
-  complement(State1,NewState,[]),
-  reverse(NewState,NewState1,[]).
-action(State1,State2,p,NewState1,NewState2):-
-  push(State1,NewState1,State2,NewState2).
-action(State1,State2,r,NewState1,NewState2):-
-  copy(State1,NewState1),
-  reverse(State2,NewState2,[]).
+action(situation(State1,[]),c,situation(State1,NewState2)):-fail.
+action(situation(State1,[H|T]),c,situation(NewState1,[H|T])):-
+  replace(85,65,71,67,State1,NewState1).
 
-all_diff(L) :- \+ (append(_,[X|R],L), memberchk(X,R)).
-remove_duplicates([],[]).
-remove_duplicates([H],[H]).
-remove_duplicates([H , P | T], List) :-
-     (H==P),
-     remove_duplicates([P|T], List).
-
-remove_duplicates([H ,P| T], [H|T1]) :-
-      \+(H==P),
-      remove_duplicates( [P|T], T1).
+action(situation([H|T],State2),p,situation(T,[H|State2])):-safe([H|State2]).
+action(situation(State1,[]),r,situation(State1,NewState2)):-fail.
+action(situation(State1,[H]),r,situation(State1,NewState2)):-fail.
+action(situation(State1,[H|T]),r,situation(State1,NewState2)):-
+  reverse([H|T],NewState2,[]).
 
 safe([]).
-safe(NewState2):-
-    remove_duplicates(NewState2,NS2),
-    all_diff(NS2).
+safe([H]).
+safe([H , P | T]) :-
+     (H==P),
+     safe([P|T]).
+safe([H ,P| T]) :-
+      not((H==P)),
+      not(memberchk(H,T)),
+      safe([P|T]).
 
-solution([],NewState2,[]).
-solution(State1, State2, [Move | Moves]) :-
-  action(State1, State2, Move, NewState1, NewState2),
-  safe(NewState2),
-  solution(NewState1,NewState2, Moves).
+solution(situation([],NewState2),[],_).
+solution(situation(State1,State2), [Move | Moves],Seen) :-
+  action(situation(State1,State2), Move, situation(NewState1, NewState2)),not(member(situation(NewState1,NewState2),Seen)),
+  solution(situation(NewState1,NewState2), Moves,[situation(NewState1,NewState2)|Seen]).
 
 solutions(Ns, Answers,[H|T]) :-
   (
     Ns == 0 -> Answers = []
   ; Ns == 1 -> length(Moves, _N),
-               solution(H,[],Moves),!,
+               once(solution(situation(H,[]),Moves,[])),
                Ns1 is Ns-1,
                string_chars(Str,Moves),
                solutions(Ns1, RestAnswers,[5,6]),
                Answers = [Str | RestAnswers]
   ; Ns > 1 -> length(Moves, _N),
-              solution(H,[],Moves),!,
+              once(solution(situation(H,[]),Moves,[])),
               string_chars(Str,Moves),
               Ns1 is Ns-1,
               solutions(Ns1, RestAnswers,T),
@@ -96,7 +80,7 @@ plin([H|T],[H1|T1],Z):-Z is H1-H.
 vaccine(File,Answers) :-
   statistics(runtime,A),
   read_input(File, Ns, Chars),
-  solutions(Ns,Answers,Chars),
+  once(solutions(Ns,Answers,Chars)),
   statistics(runtime,B),
   plin(A,B,C),
   writeln(C).
